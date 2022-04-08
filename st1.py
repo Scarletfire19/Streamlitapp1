02288de5b9fcfab0e0e9ada9db9663ba81a8f49d
Scarletfire19 <89076186+Scarletfire19@users.noreply.github.com>
2022-04-08 12:48:48 +0800

    Update st1.py
    st.bar_chart(hist_values)

--- st1.py
+++ st1.py
@@ -40,13 +40,4 @@ Xcombined=dfcombined.drop(columns=['DNA sample ethnicity and id','DNA sample eth
 dfcurrentgroup=dfcurrent.groupby(['DNA sample ethnicity']).mean().reset_index()
 Xcurrentgroup=dfcurrentgroup.drop(columns=['DNA sample ethnicity'])
 
-def PCAplot():
-  fig, ax = plt.subplots()
-  ax.scatter(dfcurrentgroup['1'], dfcurrentgroup['2'],s = 1)
-  #ax.scatter(point['1'],point['2'],s=500)
-
-  for i in range(len(dfcurrentgroup)):
-    ax.annotate(dfcurrentgroup['DNA sample ethnicity'][i], (dfcurrentgroup['1'][i], dfcurrentgroup['2'][i]))
-
-  st.pyplot
-PCAplot()
+st.map(dfcurrentgroup['1'])
