61b88525d37f89eeccf572c22a10b871032e83f9
Scarletfire19 <89076186+Scarletfire19@users.noreply.github.com>
2022-04-08 12:49:41 +0800

    Update st1.py

--- st1.py
+++ st1.py
@@ -7,23 +7,13 @@ Original file is located at
     https://colab.research.google.com/drive/1J6XoM5A3K10-5VhqbcsJWk5Ro9t_bXXD
 """
 
-pandas
-matplotlib
-pydeck
-streamlit
-flexidate
-
 import streamlit as st
 from threading import activeCount
 import streamlit.components.v1 as components
-import plotly.figure_factory as ff
 #import umap
 import io
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-
 
 dfcurrent=pd.read_csv("G25_Current_DNA.csv")
 Xcurrent=dfcurrent.drop(columns=['DNA sample ethnicity and id','DNA sample ethnicity','sample id'])
